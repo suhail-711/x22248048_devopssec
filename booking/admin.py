8990ab290853776abb51ffb815a73d6bd6247d84
suhail-711 <x22248048@student.ncirl.ie>
2025-03-08 16:23:47 +0000

    app created

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from. models import *
 
 # Register your models here.
+admin.site.register(TeamofDoctors)
+admin.site.register(Token)
+admin.site.register(Appointment)
--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class TeamofDoctors(models.Model):
+    name = models.CharField(max_length=50,null=True,blank=True)
+    specialization = models.CharField(max_length=50,null=True,blank=True)
+    op_date = models.CharField(max_length=250,null=True,blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Token(models.Model):
+    doctor = models.ForeignKey(TeamofDoctors,on_delete=models.CASCADE)
+    token_number = models.PositiveIntegerField()
+    is_booked = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.doctor.name} - {self.token_number}"
+
+class Appointment(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    contact_no = models.PositiveIntegerField(null=True)
+    doctor = models.ForeignKey(TeamofDoctors,on_delete=models.CASCADE)
+    token = models.OneToOneField(Token,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.doctor.name} - {self.token.token_number}"
+
+
+
